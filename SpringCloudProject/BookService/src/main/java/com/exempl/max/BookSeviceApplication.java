b003228ba27225ae08d98553f3d20d0ab19f53f8
Zivotovskiy <https://github.com/ZhivotovskyMaksim>
2022-11-29 02:27:59 +0500

    step_1

---
+++ SpringCloudProject/BookService/src/main/java/com/exempl/max/BookSeviceApplication.java
@@ -0,0 +1,12 @@
+package com.exempl.max;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSeviceApplication.class, args);
+	}
+}
---
+++ SpringCloudProject/BookService/src/main/java/com/exempl/max/model/Book.java
@@ -0,0 +1,23 @@
+package com.exempl.max.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String title;
+	private String author;
+	private int pages;
+}
